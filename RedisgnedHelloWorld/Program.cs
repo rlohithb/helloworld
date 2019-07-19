e419739c712ca86000a48742c19107e61deb41c2
lohith <lohithboinapally@gmail.com>
2019-07-18 22:54:17 -0400

    new

--- RedisgnedHelloWorld/Program.cs
+++ RedisgnedHelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace RedisgnedHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(1+1);
         }
     }
 }
