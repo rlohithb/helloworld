88d2199a40e7b512bddb0e95034e13fae6726599
LB <rlohithb@gmail.com>
2020-11-16 21:39:45 -0500

    New check-in

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Lohith!!!");
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+            var t = DateTime.Now;
+            Console.WriteLine($"Hello {name}, it's {t} now");
+            //Console.ReadKey(true);
+        }
+    }
+}
